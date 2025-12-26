53c2fe28de23809f3113ce4408cc18e8911392e5
akshaydivekar0624 <akshaydivekar0624@gmail.com>
2025-12-26 20:24:02 +0530

    updated new ec2 instance REQ00112233

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ad-young-minds-app-project-terraform-state"
-    key = "main"
+    key = "AD_Linux_Server_Key"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
